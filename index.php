fa42b546e607f1e6a1cf81009a819ee1073fdaee
Robson Flores <robsonflowers@gmail.com>
2017-02-02 21:20:46 -0200

    Feitas pequenas correcoes no codigo

--- index.php
+++ index.php
@@ -12,9 +12,8 @@
         <title><?php echo $title;?></title>
         <link href="<?php echo BASE_URL; ?>files/css/style.css" rel="stylesheet" type="text/css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        <script src="<?php echo BASE_URL; ?>files/js/functions.js"></script>
         <script type='text/javascript' src='https://api.stackexchange.com/js/2.0/all.js'></script>
-        <script src="<?php echo BASE_URL; ?>files/js/ajax.js"></script>
+        <script src="<?php echo BASE_URL; ?>files/js/functions.js"></script>
     </head>
     <body>
         <div class="">
--- pages/index.php
+++ pages/index.php
@@ -1,10 +1,40 @@
 <h1>PHP - StackOverflow</h1>
+<?php 
+    include_once(BASE_PATH.'app/classes/Content.dao.php');
+    include_once(BASE_PATH.'app/classes/Update.dao.php');
+
+    $lastUpdate = UpdateDAO::retornaUltimo();
+    $registros = ContentDAO::retornaTodosUltimos();
+    
+    $arrRegistros = array();
+    foreach($registros as $registros){
+        $arrRegistros[] = $registros;
+    }
+
+    $array = array(
+        "last_update" => $lastUpdate,
+        "content" => $arrRegistros,
+    );
+    
+    
+//    echo json_encode($array);
+
+?>
+
 
 <form action="" method="POST" enctype="multipart/form-data" class="persistir">
     <input type="submit" value="Persistir dados" name="persistir" id="persistir"/>
 </form>
 <div class="ver"></div>
+<div class="loader">
+    <img src="<?php echo BASE_URL?>files/images/icons/ajax_circle_loader.gif" alt=""/>
+</div>
+<div class="message"></div>
+
 <h3>Buscar na API</h3>
+
+
+
 <form action="question" method="GET" enctype="multipart/form-data" class="buscar" target="_blank">
     <!-- -->
     <label class="" for="page">Page</label>
