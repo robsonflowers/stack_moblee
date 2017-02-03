110f277aed875889cc1288856f5c4ab1dbfd1700
Robson Flores <robsonflowers@gmail.com>
2017-02-03 03:38:57 -0200

    Realizada modificacoes para testes

--- pages/index.php
+++ pages/index.php
@@ -1,52 +1,34 @@
-<h1>PHP - StackOverflow</h1>
 <?php 
     include_once(BASE_PATH.'app/classes/Content.dao.php');
     include_once(BASE_PATH.'app/classes/Update.dao.php');
-
-    $lastUpdate = UpdateDAO::retornaUltimo();
-    $registros = ContentDAO::retornaTodosUltimos();
-    
-    $arrRegistros = array();
-    foreach($registros as $registros){
-        $arrRegistros[] = $registros;
-    }
-
-    $array = array(
-        "last_update" => $lastUpdate,
-        "content" => $arrRegistros,
-    );
-    
-    
-//    echo json_encode($array);
-
 ?>
+<div class="panel">
+    <h1>PHP - StackOverflow</h1>
 
+    <form action="" method="POST" enctype="multipart/form-data" class="persistir">
+        <input type="submit" value="Persistir dados" name="persistir" class="but but-primary but-shadow but-tc" id="persistir"/>
+    </form>
 
-<form action="" method="POST" enctype="multipart/form-data" class="persistir">
-    <input type="submit" value="Persistir dados" name="persistir" id="persistir"/>
-</form>
-<div class="ver"></div>
-<div class="loader">
-    <img src="<?php echo BASE_URL?>files/images/icons/ajax_circle_loader.gif" alt=""/>
+    <div class="loader">
+        <img src="<?php echo BASE_URL?>files/images/icons/ajax_circle_loader.gif" alt=""/>
+    </div>
+
+    <div class="message"></div>
+
+    <h3>Buscar na API</h3>
+    <form action="" method="GET" enctype="multipart/form-data" class="buscar" >
+        <!-- -->
+        <label class="" for="page">Page</label>
+        <input type="text" name="page" class="ip" placeholder="">
+        <!-- -->
+        <label class="" for="rpp">RPP</label>
+        <input type="text" name="rpp" class="ip" placeholder="">
+        <!-- -->
+        <label class="" for="sort">Sort</label>
+        <input type="text" name="sort" class="ip" placeholder="">
+        <!-- -->
+        <label class="" for="score">Score</label>
+        <input type="text" name="score" class="ip" placeholder="">
+        <button type="submit" class="but but-shadow but-tc">Buscar</button>
+    </form>
 </div>
-<div class="message"></div>
-
-<h3>Buscar na API</h3>
-
-
-
-<form action="question" method="GET" enctype="multipart/form-data" class="buscar" target="_blank">
-    <!-- -->
-    <label class="" for="page">Page</label>
-    <input type="text" name="page" class="" placeholder="">
-    <!-- -->
-    <label class="" for="rpp">RPP</label>
-    <input type="text" name="rpp" class="" placeholder="">
-    <!-- -->
-    <label class="" for="sort">Sort</label>
-    <input type="text" name="sort" class="" placeholder="">
-    <!-- -->
-    <label class="" for="score">Score</label>
-    <input type="text" name="score" class="" placeholder="">
-    <button type="submit" class="">Buscar</button>
-</form>
--- pages/question.php
+++ pages/question.php
@@ -1,9 +1,38 @@
-<?php
+<?php 
+/* */
+include_once(BASE_PATH.'app/classes/Content.dao.php');
+include_once(BASE_PATH.'app/classes/Update.dao.php');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    
+    $page = (@$_GET['pag']!='')?$_GET['pag']:0;
+    $rpp = (@$_GET['rpp']!='')?$_GET['rpp']:99;
+    $sort = (@$_GET['sort']!='')?$_GET['sort']:'question_id';
+    $score = (@$_GET['score']!='')?$_GET['score']:0;
+    print_r($_GET);
+    $lastUpdate = UpdateDAO::retornaUltimo();
+    $registros = ContentDAO::retornaTodosUltimos($score, $sort, $page, $rpp);
+    
+    $arrRegistros = array();
+    foreach($registros as $registros){
+        $arrRegistros[] = $registros;
+    }
 
+    $array = array(
+        "last_update" => (int)$lastUpdate,
+        "content" => $arrRegistros,
+    );
+?>  
+<div class="titulo">
+    <h1>Resultado</h1>
+</div>
 
+<div class="resultado shadow">
+    <pre>
+        <?php echo json_encode($array,JSON_PRETTY_PRINT);?>
+    </pre> 
+</div>  
+    
+    
+    
+   
+    
