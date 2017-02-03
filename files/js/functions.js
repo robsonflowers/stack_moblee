eb2b8125b8c4508258a6fded962a35dd11bf45da
Robson Flores <robsonflowers@gmail.com>
2017-02-03 03:40:39 -0200

    Adicionadas funcoes no arquivo files/js/functions.js

--- files/js/functions.js
+++ files/js/functions.js
@@ -2,10 +2,21 @@
 /* FUNÇÃO PARA BUSCAR OS DADOS PELA API STACKEXCHANGE */
 $(function(){
     $('.persistir').submit(function(){
-        $.getJSON('https://api.stackexchange.com/2.2/search?pagesize=99&order=asc&sort=creation&tagged=php&site=stackoverflow', 
+        /* RETORNA O TEMPO ATUAL EM FORMATO UNIX */
+        var timestamp = $.now();
+        var unix = timestamp.toString();
+        unix = unix.substring(0, 10);
+        /* ESVAZIA A TABELA PARA INSERIR REGISTROS ATUALIZADOS */
+        $.ajax({ 
+            type: 'GET',
+            dataType: 'html', 
+            url: 'http://localhost/stack_moblee/app/ajax/ajax.php',
+            data: 'limpartabela',
+            success: function() {}
+        });
+        $.getJSON('https://api.stackexchange.com/2.2/search?pagesize=99&todate='+unix+'&order=desc&sort=creation&tagged=php&site=stackoverflow', 
         function(data){
             $.each(data.items, function(key, result){
-//                $('.ver').append(key+' -> ID: '+result.question_id+' - titulo: '+addslashes(result.title)+' - nome: '+result.owner.display_name+' - score: '+result.score+' - criacao: '+result.creation_date+' - resp: '+result.is_answered+'<br>');
                 var question_id = result.question_id;
                 var title = result.title;
                 var owner_name = result.owner.display_name;
@@ -16,35 +27,42 @@ $(function(){
                 /* Funcao responsavel por enviar os dados coletados para o arquivo 
                  * onde sera realizada a persistencia dos mesmos */
                 $.ajax({ 
-                        type: 'GET',
-                        dataType: 'html', 
-                        url: 'http://localhost/stack_moblee/app/ajax/ajax.php',
-                        data: 'questionid='+question_id+'&title='+title+'&ownername='+owner_name+
-                              '&score='+score+'&creationdate='+creation_date+'&link='+link+'&isanswered='+is_answered,
-                        success: function(resposta) {
-                            var texto = 'teste';
-//                            alert(title);
-//                            alert(resposta);
-//                            if(resposta==='ok'){
-//                                texto = "Dados salvos localmente";
-//                            }
-//                            else{
-//                                texto = "Ocorreu um erro ao tentar salvar seus dados no Banco de Dados!";
-//                            }
-                            $('.message').text(texto);
-                        },
-                        beforeSend: function(){
-                           $('.loader').css({display:"block"});
-                        },
-                        complete: function(){
-                           $('.loader').css({display:"none"});
-                        },
-                         error: function(XMLHttpRequest, textStatus, errorThrown){
-                            $('.message').text("Ocorreu um erro na requisicao do AJAX, culpe o Deadpool!");
+                    type: 'GET',
+                    dataType: 'html', 
+                    url: 'http://localhost/stack_moblee/app/ajax/ajax.php',
+                    data: 'questionid='+question_id+'&title='+title+'&ownername='+owner_name+
+                          '&score='+score+'&creationdate='+creation_date+'&link='+link+'&isanswered='+is_answered,
+                    success: function(resposta) {
+                        var texto;
+                        if(resposta==='ok'){
+                            texto = "Dados salvos localmente";
                         }
-                    });
+                        else{
+                            texto = "Ocorreu um erro ao tentar salvar um ou mais dados no Banco de Dados!";
+                        }
+                        $('.message').text(texto).css({display:"block"});
+                    },
+                    beforeSend: function(){
+                       $('.loader').css({display:"block"});
+                    },
+                    complete: function(){
+                       $('.loader').css({display:"none"});
+                    },
+                     error: function(XMLHttpRequest, textStatus, errorThrown){
+                        $('.message').text("Ocorreu um erro na requisicao do AJAX, culpe o Deadpool!");
+                    }
+                });
             });
         });
+        return false;
+    });
+});
+
+/* FUNÇÃO PARA ABRIR NOVA ABA COM AS CONDICOES DE BUSCA */
+$(function(){
+    $('.buscar').submit(function(){
+        var dados = $('.buscar').serialize();
+        window.open('http://localhost/stack_moblee/?pagina=question&'+dados, '_blank');
         return false;
     });
 });
