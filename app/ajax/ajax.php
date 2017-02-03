e17dcb040ede08b6b837a7d18ab4b4f6dfaac8b3
Robson Flores <robsonflowers@gmail.com>
2017-02-03 03:41:57 -0200

    Realizadas melhorias no código do arquivo app/ajax/ajax.php

--- app/ajax/ajax.php
+++ app/ajax/ajax.php
@@ -1,12 +1,16 @@
 
 <?php 
 /* Pagina para manipulacao de dados via ajax*/
+include_once('../classes/Utils.php');
 include_once('../classes/Content.dao.php');
 include_once('../classes/Update.dao.php');
+if(isset($_GET['limpartabela'])){
+    ContentDAO::delete();
+}
 if(isset($_GET['questionid'])){
     
     $questionid = (isset($_GET['questionid']))?$_GET['questionid']:'';
-    $title = addslashes($_GET['title']);
+    $title = Utils::cleanWords($_GET['title']);
     $ownername = (isset($_GET['ownername']))?$_GET['ownername']:'';
     $score = (isset($_GET['score']))?$_GET['score']:'';
     $creationdate = (isset($_GET['creationdate']))?$_GET['creationdate']:'';
@@ -14,9 +18,7 @@ if(isset($_GET['questionid'])){
     $isanswered = (isset($_GET['isanswered']))?(($_GET['isanswered']==true)?1:0):0;
     
     $insert = ContentDAO::insere($questionid, $title, $ownername, $score, $creationdate, $link, $isanswered);
-//    echo $insert;
     UpdateDAO::insere();
-    
 }    
     
     
